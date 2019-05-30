03218f64b6bc149f0f484b2bc2656ea132b34f42
az <azizkoura@hotmail.com>
2019-05-30 23:20:15 +0200

    Multiple Router Views(Named Router Views)

--- src/routes.js
+++ src/routes.js
@@ -3,10 +3,17 @@ import UserStart from './components/user/UserStart'
 import UserDetail from './components/user/UserDetail'
 import UserEdit from './components/user/UserEdit'
 import Home from './components/Home'
+import Header from './components/Header'
 
 export const routes = [
-    { path: '', component: Home, name: 'home' },
-    { path: '/user', component: User, children: [
+    { path: '', name: 'home', components: {
+        default: Home,
+        'header-top': Header
+    } },
+    { path: '/user', components: {
+        default: User,
+        'header-bottom': Header
+    }, children: [
         { path: '', component: UserStart },
         { path: ':id', component: UserDetail },
         { path: ':id/edit', component: UserEdit, name: 'userEdit' }
