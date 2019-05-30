6488bda8a1280149db46888ccd2a2563585da35a
az <azizkoura@hotmail.com>
2019-05-30 22:24:21 +0200

    setting Up child routes (nested Routes

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,14 @@
 import User from './components/user/User'
+import UserStart from './components/user/UserStart'
+import UserDetail from './components/user/UserDetail'
+import UserEdit from './components/user/UserEdit'
 import Home from './components/Home'
 
 export const routes = [
     { path: '', component: Home },
-    { path: '/user/:id', component: User }
+    { path: '/user', component: User, children: [
+        { path: '', component: UserStart },
+        { path: ':id', component: UserDetail },
+        { path: ':id/edit', component: UserEdit }
+    ] }
 ]
