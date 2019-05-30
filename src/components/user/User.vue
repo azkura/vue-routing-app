b081a4befee092ef9f5e8b84bff6ac53184ee9a4
az <azizkoura@hotmail.com>
2019-05-30 20:48:47 +0200

    Fetching and using Routes Parameters

--- src/components/user/User.vue
+++ src/components/user/User.vue
@@ -2,12 +2,18 @@
     <div>
         <h1>The User Page</h1>
         <hr>
+        <p>LOADED ID: {{ id }}</p>
         <button @click="navigateToHome" class="btn btn-success">Go to Home</button>
     </div>
 </template>
 
 <script>
 export default {
+    data() {
+        return {
+            id: this.$route.params.id
+        }
+    },
     methods: {
         navigateToHome() {
             this.$router.push('/')
